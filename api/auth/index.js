564e68c0aac75f5c234eabf6395fbfcab9308bb1
Bunk <jd.courtoy@gmail.com>
2016-01-02 09:31:13 -0600

    Refactored out an auth controller for the business logic

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,20 +1,12 @@
 'use strict';
 
-const UUID = require('node-uuid');
-const JWT = require('jsonwebtoken');
+const co = require('co');
+const AuthController = require('./controller');
 
 exports.register = function (server, options, next) {
 
-    server.auth.strategy('jwt', 'jwt', {
-        key: options.jwt.secret,
-        validateFunc: (decoded, request, callback) => {
-            // TODO: Database lookup to enable revocation of tokens
-            return callback(null, true); // success
-        },
-        verifyOptions: {
-            algorithms: [ 'HS256' ]
-        }
-    });
+    let controller = new AuthController(options);
+    server.bind(controller);
 
     // Setup the Google login strategy
     server.auth.strategy('google', 'bell', {
@@ -25,52 +17,23 @@ exports.register = function (server, options, next) {
         isSecure: false //Should be set to true (which is the default) in production
     });
 
-    server.auth.default('jwt');
+    // Main bearer token implementation
+    server.auth.strategy('bearer', 'jwt', {
+        key: options.jwt.secret,
+        validateFunc: controller.validateToken,
+        verifyOptions: {
+            algorithms: [ 'HS256' ]
+        }
+    });
+    server.auth.default('bearer');
 
     server.route({
         method: 'GET',
         path: '/auth/google',
         config: {
             auth: 'google',
-            handler: function (request, reply) {
-
-                if (!request.auth.isAuthenticated) {
-                    return reply(Boom.unauthorized('Authentication failed: ' + request.auth.error.message));
-                }
-
-                //Just store the third party credentials in the session as an example. You could do something
-                //more useful here - like loading or setting up an account (social signup).
-                //request.auth.session.set(request.auth.credentials);
-
-                let now = new Date().getTime();
-                let profile = request.auth.credentials.profile;
-                let session = {
-                    jti: UUID.v4(),
-                    iat: now,                       // Issuance date
-                    exp: now + 24 * 60 * 60 * 1000, // Expires in 1 day
-                    scopes: {
-                        resumes: {
-                            actions: ['read', 'create']
-                        },
-                        conversions: {
-                            actions: ['read', 'create']
-                        },
-                        templates: {
-                            actions: ['read', 'create']
-                        }
-                    },
-                    profile: {
-                        name: profile.displayName,
-                        email: profile.emails[0].value,
-                        avatar: profile.raw.image
-                    }
-                };
-
-                // sign the session as a JWT
-                let token = JWT.sign(session, options.jwt.secret); // synchronous
-                console.log(token);
-
-                return reply({ token: token }).header('Authorization', `Bearer ${token}`);
+            handler: {
+                async: co.wrap(controller.authorizeGoogle)
             }
         }
     });
@@ -91,5 +54,5 @@ exports.register = function (server, options, next) {
 exports.register.attributes = {
     name: 'auth',
     version: '0.0.1',
-    dependencies: ['hapi-auth-cookie', 'hapi-auth-jwt2', 'bell']
+    dependencies: ['hapi-auth-jwt2', 'bell']
 };
